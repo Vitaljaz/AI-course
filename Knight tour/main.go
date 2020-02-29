9963e2fa26f8c78ec9aa508946a5fbd960103bd0
Vitaljaz <43145747+Vitaljaz@users.noreply.github.com>
2020-02-29 13:23:47 +0300

    create interface for user

--- Knight tour/main.go
+++ Knight tour/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
+	var N int
+	var startX, startY int
+	var endX, endY int
 
+	fmt.Println("Enter N:")
+	fmt.Scan(&N)
+
+	fmt.Println("Enter start position (x;y): ")
+	fmt.Scan(&startX, &startY)
+
+	fmt.Println("Enter end position (x;y): ")
+	fmt.Scan(&endX, &endY)
 }
