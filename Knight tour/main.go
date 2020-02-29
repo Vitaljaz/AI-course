e4b0a4eb7ea2292615ca9e95c85092f30de39c83
Vitaljaz <43145747+Vitaljaz@users.noreply.github.com>
2020-02-29 14:30:41 +0300

    create bruteforce solution

--- Knight tour/main.go
+++ Knight tour/main.go
@@ -6,27 +6,49 @@ import (
 )
 
 var (
-	N int
-	startX int
+	n int
+	startX = 100
 	startY int
 	endX int
 	endY int
 )
 
 func RunBruteForce() {
+	fmt.Println("[BruteForce]:")
 	timer := time.Now()
-	BruteForce(N, startX, startY, endX, endY)
+	var current int
+	chess := CreateChess()
+	BruteForce(chess, current, startX, startY)
 	fmt.Printf("[BruteForce]: Elapsed time: %v", time.Since(timer))
 }
 
+func PrintChess(chess [][]int) {
+	chess[startX][startY] = -1
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			fmt.Printf("%3d", chess[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func CreateChess() [][] int {
+	chess := make([][]int, n)
+	for i := range chess {
+		chess[i] = make([]int, n)
+	}
+	return chess
+}
 
 func main() {
 	fmt.Println("Enter N:")
-	fmt.Scan(&N)
+	fmt.Scan(&n)
 
 	fmt.Println("Enter start position (x;y): ")
 	fmt.Scan(&startX, &startY)
 
 	fmt.Println("Enter end position (x;y): ")
 	fmt.Scan(&endX, &endY)
+
+	RunBruteForce()
 }
