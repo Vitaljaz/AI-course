1eed7255da007f310ae1e42dda0261b3d0050236
Vitaljaz <43145747+Vitaljaz@users.noreply.github.com>
2020-03-03 21:52:14 +0300

    refractoring

--- Knight tour/main.go
+++ Knight tour/main.go
@@ -7,30 +7,6 @@ import (
 )
 
 var (
-	dx = [8]int {1, 1, -1, -1, 2, 2, -2, -2}
-	dy = [8]int {2, -2, 2, -2, 1, -1, 1, -1}
-)
-
-type Vertex struct {
-	x int
-	y int
-
-	parent *Vertex
-}
-
-func (v *Vertex) Create(x, y int) {
-	v.x = x
-	v.y = y
-}
-
-func IsValidMove(v *Vertex) bool {
-	if v.x < 0 || v.x >= n || v.y < 0 || v.y >= n {
-		return false
-	}
-	return true
-}
-
-var (
 	n 	   int
 	startX int
 	startY int
@@ -39,6 +15,9 @@ var (
 	destroyCount     int
 	destroyFieldsX []int
 	destroyFieldsY []int
+		
+	dx = [8]int {1, 1, -1, -1, 2, 2, -2, -2}
+	dy = [8]int {2, -2, 2, -2, 1, -1, 1, -1}
 )
 
 func RunBruteForce() {
