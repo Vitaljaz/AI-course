a83ea074a38eb57b75bd322d473fefb5845f2d50
Vitaljaz <43145747+Vitaljaz@users.noreply.github.com>
2020-03-03 21:42:01 +0300

    update algorithm

--- Knight tour/bfs.go
+++ Knight tour/bfs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"container/list"
+	"fmt"
 )
 
 var (
@@ -14,11 +15,29 @@ func GetFromList() *Vertex {
 	return v
 }
 
-func BFS(chess [][]int) bool {
+func RestorePath(vertex *Vertex, chess [][]int) [][]int {
+	chess[endX][endY] = -2
+	v := vertex.parent
+	i := 1
+	fmt.Println("[BFS]: Reverse Path: ")
+	fmt.Printf("(%d;%d) ", endX, endY)
+	for v != nil {
+		chess[v.x][v.y] = i
+		i++
+		fmt.Printf("(%d;%d) ", v.x, v.y)
+		v = v.parent
+	}
+	fmt.Printf("\n")
+	return chess
+}
+
+func BFS(chess [][]int) (can bool, finish *Vertex) {
 	vStart := new(Vertex)
 	vStart.Create(startX, startY)
-	current := 1
 	l.PushBack(vStart)
+
+	can = false
+	chess[startX][startY] = -1
 
 	for l.Len() != 0 {
 		node := GetFromList()
@@ -27,8 +46,24 @@ func BFS(chess [][]int) bool {
 		for k := 0; k < 8; k++ {
 			next := new(Vertex)
 			next.Create(node.x + dx[k], node.y + dy[k])
+
+			if !IsValidMove(next) {
+				continue
+			}
+
+			if chess[next.x][next.y] == 0 &&
+				chess[next.x][next.y] != -3 {
+					next.parent = node
+
+					if next.x == endX && next.y == endY {
+						can = true
+						finish = next
+						return
+					}
+
+					l.PushBack(next)
+				}
 		}
 	}
-
-	 return false
+	 return
 }
